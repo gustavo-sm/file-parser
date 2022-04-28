e16ee9bacc4501ec6f97c04ec7c13045e5f8fe08
gus-sm <gustavo.mashiba@bexpert.com.br>
2022-04-28 17:15:29 -0300

    Segunda refatoração do codigo

--- index.js
+++ index.js
@@ -2,29 +2,28 @@ import File from  './src/File.js';
 import Parser from './src/Parser.js';
 import config from './config/config.js';
 import consoleMessage from './utils/console-message.js';
+import FileOperations from './src/FileOperations.js';
 
 const filePath = process.argv[2], 
 option = process.argv[3] || 'p';
 
 try{
-    const file = new File(filePath);
+    const FileOps = new FileOperations(),
+    file = new File(filePath, FileOps),
+    parser = new Parser(file.extractContent());
 
-    file.read();
-
-    const parser = new Parser(file.getFileContent());
     let content = '';
 
     const mode = {
         p(){
             parser.parse();
-            console.log(parser.getParsedContent());
             content = parser.getParsedContent();
-            file.write(config.PARSED_FILENAME, content);
+            FileOps.write(config.PARSED_FILENAME, content);
         },
         u(){
             parser.unparse();
             content = parser.getUnparsedContent();
-            file.write(config.UNPARSED_FILENAME, content);
+            FileOps.write(config.UNPARSED_FILENAME, content);
         }
     };
     mode[option]();
--- src/File.js
+++ src/File.js
@@ -1,31 +1,13 @@
-import fs from 'fs';
-import config from '../config/config.js';
-
 class File {
 
-    constructor(readPath){
+    constructor(readPath, FileOps){
         this.fileContent = '';
         this.readPath = readPath;
-        this.writePath = config.DEST_PATH;
+        this.FileOps = FileOps;
     }
 
-    read(){
-        if (!fs.existsSync(this.readPath)){
-            throw new Error('Arquivo não encontrado!');
-        }
-        const fileContent = fs.readFileSync(this.readPath, 'utf-8');
-        this.fileContent = fileContent;
-
-    }
-
-    write(filename, content){
-        if(!fs.existsSync(this.writePath.split('/')[1])){
-            fs.mkdirSync(this.writePath.split('/')[1]);
-        }
-        fs.writeFileSync(`${this.writePath}/${filename}`, content);
-    }
-
-    getFileContent(){
+    extractContent(){
+        this.fileContent = this.FileOps.read(this.readPath);
         return this.fileContent;
     }
 
